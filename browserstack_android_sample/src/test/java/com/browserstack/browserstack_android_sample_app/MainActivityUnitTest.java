2f3a5697b86cc1498b1fc3eaa5f2ada42c3580aa
Pankaj Ahuja <ray.pankaj@gmail.com>
2018-09-14 15:49:28 +0530

    user details

--- browserstack_android_sample/src/test/java/com/browserstack/browserstack_android_sample_app/MainActivityUnitTest.java
+++ browserstack_android_sample/src/test/java/com/browserstack/browserstack_android_sample_app/MainActivityUnitTest.java
@@ -27,12 +27,12 @@ public class MainActivityUnitTest {
     capabilities.setCapability("name", "single_appium_test");
     capabilities.setCapability("browserstack.debug", true);
 
-    String username = System.getenv("BROWSERSTACK_USER");
+    String username = "pankajahuja1";
 
-    String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    String accessKey = "aCxhDStUopf8tsyz4d55";
 
     // App url of the uploaded app on BrowserStack.
-    String app = System.getenv("BROWSERSTACK_APP_ID");
+    String app = "bs://35d129dfc8b5223d2e6a65e44dab6fbbedf4e1a3";
     System.out.println("USER" + username);
     System.out.println("KEY" + accessKey);
     System.out.println("App ID" + app);
