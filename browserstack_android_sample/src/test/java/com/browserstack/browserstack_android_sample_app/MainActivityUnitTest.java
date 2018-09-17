004db51bf1ed8be9e738acbcaa1d9ba4007af8b4
Pankaj Ahuja <ray.pankaj@gmail.com>
2018-09-17 11:58:45 +0530

    user to username

--- browserstack_android_sample/src/test/java/com/browserstack/browserstack_android_sample_app/MainActivityUnitTest.java
+++ browserstack_android_sample/src/test/java/com/browserstack/browserstack_android_sample_app/MainActivityUnitTest.java
@@ -27,7 +27,7 @@ public class MainActivityUnitTest {
     capabilities.setCapability("name", "single_appium_test");
     capabilities.setCapability("browserstack.debug", true);
 
-    String username = System.getenv("BROWSERSTACK_USER");
+    String username = System.getenv("BROWSERSTACK_USERNAME");
 
     String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
 
